5552ac8a5dea091f4e7cd19bf2ae331a35fbcb98
Osama Sakhi <msakhi21@gmail.com>
2016-11-28 01:36:47 -0500

    Mostly done. Need actually mp4 file

---
+++ osamasakhi_files/js/init.js
@@ -0,0 +1,16 @@
+(function($) {
+
+	$(function() {
+
+
+	$('.button-collapse').sideNav({
+		menuWidth: 240, // Default is 240
+		edge: 'left', // Choose the horizontal origin
+		closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+	});
+
+	$('.parallax').parallax();
+
+	}); // end of document ready
+
+})(jQuery); // end of jQuery name space
